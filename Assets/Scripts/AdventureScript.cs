07ab1fa24d92f24991fdce1291ad4b6a85f26ff2
Anime Lover <bawagupta69@gmail.com>
2019-12-14 20:37:10 +0530

    First Commit

---
+++ Assets/Scripts/AdventureScript.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureScript : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        // The string inside the textComponent field
+        textComponent.text = state.GetStoryState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            state = nextStates[0];
+        }
+
+        for(int index=0; index<nextStates.Length; index++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+        
+        textComponent.text = state.GetStoryState();
+    }
+}
